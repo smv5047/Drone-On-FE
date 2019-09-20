e3519f360dc7443ce766431eb6286a2846b8dd84
smv5047 <smv5047@gmail.com>
2019-09-19 21:43:03 -0400

    added Slider component

---
+++ src/components/RenterCarousel.js
@@ -0,0 +1,13 @@
+import Slide from 'react-slick'
+
+export default function RenterCarousel (props) {
+
+
+
+    return (
+        <Slider>
+            <img src={props.url}/>
+            <h3>{props.name}</h3>
+        </Slider>
+    );
+}
