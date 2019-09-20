e56ba9760e2873cae24200de4732c11a00588fca
Dennis Padiernos <depadiernos@gmail.com>
2019-09-20 13:17:46 -0500

    add onboarding route and fix some imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Route } from 'react-router-dom'
-import Home from 'pages/Home'
-import AuthView from 'pages/AuthView';
-import renterView from 'pages/renterview';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "pages/Home";
+import AuthView from "pages/AuthView";
+import RenterView from "pages/RenterView";
 
 function App() {
   return (
     <div className="App">
-      <Route path='/' exact component={Home} />
-      <Route path='/signup' render={(props) => <AuthView {...props} signup /> } />
-      <Route path='/login' component={AuthView} />
-      <Route path='/find-drone' component={renterView} />
-
+      <Route path="/" exact component={Home} />
+      <Route path="/signup" render={props => <AuthView {...props} signup />} />
+      <Route path="/login" component={AuthView} />
+      <Route
+        path="/onboarding"
+        render={props => <Home {...props} onboarding />}
+      />
+      <Route path="/find-drone" component={RenterView} />
     </div>
   );
 }
