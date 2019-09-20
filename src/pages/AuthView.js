9bc78a24ff4420a01f133a0db0fb619404331550
Dennis Padiernos <depadiernos@gmail.com>
2019-09-19 21:20:38 -0500

    get basic auth form implemented

--- src/pages/AuthView.js
+++ src/pages/AuthView.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import axios from 'utils/Axios'
-import AuthView from 'components/AuthForm'
+import AuthForm from 'components/AuthForm'
 
-export default function AuthView() {
+export default function AuthView(props) {
     const [user, setUser] = useState()
+
     return (<div>
-        <AuthView />
-        <button>Login</button>
-        <button>Signup</button>
+        <AuthForm user={user} setUser={setUser} signup={props.signup}/>
+        <NavLink to='/login'><button>Login</button></NavLink>
+        <NavLink to='/signup'><button>Signup</button></NavLink>
     </div>)
 }
