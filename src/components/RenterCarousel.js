100b65cfc5d90650a32b21ec3a2046478f2c245b
smv5047 <smv5047@gmail.com>
2019-09-22 17:57:41 -0400

    Renter carousel working with dummy data

--- src/components/RenterCarousel.js
+++ src/components/RenterCarousel.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import Slider from 'react-slick'
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 export default function RenterCarousel (props) {
     console.log(props.drones)
-    
+    const settings = {
+        dots: true,
+        
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    };
 
     return (
-        <Slider>
-
-            <img src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
-            <h3>{props.drones && props.drones.name}</h3>
+        <Slider {...settings} >
+            <div className="renter carouselCard">
+                <img height={100} width={100}  src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
+                <h3>{props.drones && props.drones.name}</h3>
+            </div>
+            <div className="renter carouselCard">
+                <img height={100} width={100}  src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
+                <h3>{props.drones && props.drones.name}</h3>
+            </div>
+            <div className="renter carouselCard">
+                <img height={100} width={100} src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
+                <h3>{props.drones && props.drones.name}</h3>
+            </div>
+            <div className="renter carouselCard">
+                <img height={100} width={100} src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
+                <h3>{props.drones && props.drones.name}</h3>
+            </div>
+            <div className="renter carouselCard">
+                <img height={100} width={100}src={props.drones && props.drones.image} alt={props.drones && props.drones.name}/>
+                <h3>{props.drones && props.drones.name}</h3>
+            </div>
         </Slider>
     );
 }
