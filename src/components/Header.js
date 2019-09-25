d35b869b8b6fdc28e372b43d7f67febfd706fb6a
Dennis Padiernos <depadiernos@gmail.com>
2019-09-24 20:37:04 -0500

    make app look more like marketing site

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react"
+import styled from "styled-components"
+
+const Style = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #172233;
+  h1 {
+    color: #eae0d5;
+    font-size: 4rem;
+    span{
+      color: #f28818;
+    }
+  }
+`
+
+export default function Dashboard() {
+  return (
+    <Style>
+      <div>
+      <h1>
+        <img src={`${process.env.PUBLIC_URL}/drone.svg`} /> Drone<span>On</span>
+      </h1>
+
+      </div>
+    </Style>
+  )
+}
