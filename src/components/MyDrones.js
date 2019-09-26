28f8c189f183a5001589c97191788a19452d83db
Dennis Padiernos <depadiernos@gmail.com>
2019-09-26 17:23:52 -0500

    add title

--- src/components/MyDrones.js
+++ src/components/MyDrones.js
@@ -4,6 +4,7 @@ export default function MyDrones({myDrones}) {
  
   return (
     <div>
+      <h1>My Drones</h1>
       {myDrones &&
         myDrones.map((drone) => <div key={drone.id}>{drone.Name}</div>)}
     </div>
--- src/components/RentedDrones.js
+++ src/components/RentedDrones.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-export default function RentedDrones({rentedDrones}) {
- 
+export default function RentedDrones({ rentedDrones }) {
   return (
     <div>
+      <h1>Drones I have rented</h1>
       {rentedDrones &&
         rentedDrones.map((drone) => <div key={drone.id}>{drone.Name}</div>)}
     </div>
