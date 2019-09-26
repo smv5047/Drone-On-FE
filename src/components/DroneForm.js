a780db04baccfa808a9584b358857d534cc15bf5
Dennis Padiernos <depadiernos@gmail.com>
2019-09-26 15:04:56 -0500

    add validation for droneform

--- src/components/DroneForm.js
+++ src/components/DroneForm.js
@@ -1,7 +1,10 @@
 import React from "react"
 import styled from "styled-components"
+import { withFormik, Form, Field } from "formik"
+import * as yup from "yup"
+import axios from "utils/Axios"
 
-const Style = styled.form.attrs()`
+const Style = styled.div.attrs()`
   margin: 1rem;
   min-height: 8rem;
   display: flex;
@@ -9,7 +12,7 @@ const Style = styled.form.attrs()`
   align-items: center;
   justify-items: center;
 `
-export default function DroneForm(props) {
+function DroneForm(props) {
   const { vendorDrone, setVendorDrone, onSubmit } = props
 
   const onChange = (event) => {
@@ -17,55 +20,54 @@ export default function DroneForm(props) {
   }
 
   return (
-    <Style onSubmit={onSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Drone Name"
-        value={vendorDrone.name}
-        onChange={onChange}
-      />
-
-      <input
-        type="text"
-        name="model"
-        placeholder="Drone Model"
-        value={vendorDrone.model}
-        onChange={onChange}
-      />
-      <input
-        type="text"
-        name="brand"
-        placeholder="Drone Brand"
-        value={vendorDrone.brand}
-        onChange={onChange}
-      />
-      <input
-        type="text"
-        name="purpose"
-        placeholder="Purpose"
-        value={vendorDrone.purpose}
-        onChange={onChange}
-      />
-
-      <input
-        type="text"
-        name="experience"
-        placeholder="Experience Level"
-        value={vendorDrone.experience}
-        onChange={onChange}
-      />
-      <input
-        type="number"
-        min="0.00"
-        max="10000.00"
-        step="0.01"
-        name="cost"
-        placeholder="Rental Cost"
-        value={vendorDrone.cost}
-        onChange={onChange}
-      />
-      <button type="submit">Submit</button>
+    <Style>
+      <h2>List your Drone</h2>
+      <p>You have a drone you want to rent out. That's GREAT! Please provide the following information about your drone.</p>
+      <Form>
+        <Field type="text" name="name" placeholder="Drone Name" /><br/>
+        <Field type="text" name="model" placeholder="Drone Model" /><br/>
+        <Field type="text" name="brand" placeholder="Drone Brand" /><br/>
+        <Field type="text" name="purpose" placeholder="Purpose" /><br/>
+        <Field type="text" name="experience" placeholder="Experience Level" /><br/>
+        <Field
+          type="number"
+          min="0.00"
+          max="10000.00"
+          step="0.01"
+          name="cost"
+          placeholder="Rental Cost"
+        /><br/>
+        <button type="submit">Submit</button>
+      </Form>
     </Style>
   )
 }
+
+export default withFormik({
+  mapPropsToValues({ name, model, brand, purpose, experience, cost }) {
+    return {
+      name: name || "",
+      model: model || "",
+      brand: brand || "",
+      purpose: purpose ||"",
+      cost: cost||"" 
+    }
+  },
+  validationSchema: yup.object().shape({
+    name: yup.string().required("Please provide a name for your drone"),
+    model: yup.string().required("Please provide the model for your drone"),
+    brand: yup.string().required("Please provide the brand for your drone"),
+    purpose: yup.string().required("Please provide the best way to use your drone"),
+    cost: yup.number().required("Please provide how much it cost to rent per day")
+  }),
+  handleSubmit(values, { props, setStatus, resetForm, setSubmitting }) {
+    const { saveToken } = props
+    axios()
+      .post("https://droneon.herokuapp.com/api/drones", values)
+      .then((res) => {
+        saveToken(res.data)
+        props.history.push("/")
+      })
+      .catch((err) => console.log(err))
+  }
+})(DroneForm)
