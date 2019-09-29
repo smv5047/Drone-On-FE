88d028d3aba147ab60e36410c3bdb17208677e29
smv5047 <smv5047@gmail.com>
2019-09-29 18:14:32 -0400

    created logout function

--- src/pages/SettingsView.js
+++ src/pages/SettingsView.js
@@ -2,6 +2,8 @@ import React, { useContext, useState } from "react"
 import styled from 'styled-components'
 import EditUserForm from "components/EditUserForm"
 import { CurrentUser } from "../App"
+import {Route} from "react-router-dom"
+import AuthView from "pages/AuthView"
 
 const Style = styled.div`
   display: flex;
@@ -10,20 +12,34 @@ const Style = styled.div`
   justify-content: center;
 `
 
+
+
 export default function Home() {
   const { authUser, setAuthUser } = useContext(CurrentUser)
   const [isEditing, setIsEditing] = useState(false)
+
+  /*Logout Function*/
+  function Logout () {
+    localStorage.clear()
+    setAuthUser(null)
+  }
   return (
     <div>
       {!isEditing ? (
         <Style>
           <p>{authUser.full_name}</p><p>{authUser.email}</p>
           <button onClick={() => setIsEditing(true)}>Edit</button>
+          <br/>
+          <button onClick={() => Logout()}>Log Out</button>
+          <br/>
         </Style>
       ) : (
         <Style>
         <EditUserForm setIsEditing={setIsEditing} />
         <button onClick={() => setIsEditing(false)}>Cancel</button>
+        <br/>
+        <button onClick={() => Logout()}>Log Out</button>
+        <br/>
         </Style>
       )}
     </div>
